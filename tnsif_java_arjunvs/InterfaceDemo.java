4910e844134e164672f9f1ed87e894165d08ed2c
arjunvs <arjunvs23122004@gmail.com>
2025-10-09 14:41:16 +0530

    Create InterfaceDemo.java

---
+++ tnsif_java_arjunvs/InterfaceDemo.java
@@ -0,0 +1,23 @@
+package com.tnsif.interfacedemo;
+
+ interface Shape{
+	 
+	void draw();
+}
+
+public class InterfaceDemo implements Shape{
+	
+	public void draw() {
+		System.out.println("Drawing a circle in an interface");
+	}
+
+	public static void main(String[] args) {
+	
+		InterfaceDemo id = new InterfaceDemo();
+		id.draw();
+		
+
+		
+	}
+
+}
