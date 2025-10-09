57c4f3fb6c1ab9f061374c6c4c4f735a7ce49110
arjunvs <arjunvs23122004@gmail.com>
2025-10-09 14:44:28 +0530

    Create StaticDemo.java

---
+++ tnsif_java_arjunvs/StaticDemo.java
@@ -0,0 +1,27 @@
+package com.tnsif.nonaccessmodifiers;
+
+public class StaticDemo {
+	
+	static int count;	
+	
+	static {
+		count = 20;
+		System.out.println("shows the static count assigned: " + count);
+	}
+	
+	static void show() {
+		System.out.println("Method runs in static with count of: " + count);
+	}
+	
+
+	public static void main(String[] args) {
+	
+		System.out.println("static in main method: "+count);
+		
+		show();
+		
+		
+
+	}
+
+}
